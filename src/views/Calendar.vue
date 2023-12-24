585fb2ce82b5b048cab856c1f85e8eeab542e6cf
Ignacio Daniel García <igniciagar@gmail.com>
2023-12-24 19:19:10 -0300

    agrego FullCalendar primera vez, falta conectar con firebase

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,11 +1,58 @@
 <script>
+  import FullCalendar from '@fullcalendar/vue3';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import listPlugin from '@fullcalendar/list';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import esLocale from '@fullcalendar/core/locales/es';
+
+  import Navbar from '../components/Navbar.vue';
+  import Footer from '../components/Footer.vue';
+  
+
+  export default {
+    components: {
+      FullCalendar,
+      Navbar,
+      Footer
+    },
+    data() {
+      return {
+        calendarOptions: {
+          plugins: [dayGridPlugin,timeGridPlugin,listPlugin],
+          initialView: 'dayGridMonth',
+          headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,dayGridDay,listWeek',
+          },
+          titleFormat: { year: 'numeric', month: 'long' },
+          weekends: true,
+          locale: esLocale,
+          events: [
+            { title: 'Pilates 19hs', start: '2023-12-24',
+            backgroundColor: 'red',
+            textColor: 'white'},
+            { title: 'Yogaaa eee', start: new Date() },
+          ]
+       }
+     }
+   }
+ }
 
 </script>
-
 <template>
-
+    <Navbar />
+    <br><br><br><br><br><br><br>
+    <FullCalendar :options="calendarOptions" />
+    <br><br><br><br><br><br><br>
+    <Footer />
 </template>
 
 <style  scoped>
-
+.fc {
+    padding-top: 20px;
+    background-color: #111;
+    color: #ffa44e;
+    font-size: 11px;
+}
 </style>
